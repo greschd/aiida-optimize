4738447462c8c3f4bf37882730c5decd96c8ca8a
Austin Zadoks <zooks97@gmail.com>
2020-10-26 16:46:29 +0100

    Add support for nested-key syntax in OptimizationWorkChain
    Adds support for the nested-key syntax in the input keys that
    engines pass to the OptimizationWorkChain
    
    For outputs, a `get_nested_result` helper function is added, to
    be used by the engine implementations.

--- aiida_optimize/_utils.py
+++ aiida_optimize/_utils.py
@@ -2,11 +2,24 @@
 """
 Defines common helper functions.
 """
-
 import typing as ty
 
 from aiida import orm
 from aiida.common.links import LinkType
+from aiida.orm.nodes.data.base import to_aiida_type
+
+
+def from_aiida_type(value):
+    """
+    Convert an AiiDA data object to the equivalent Python object
+    """
+    if isinstance(value, (orm.BaseType)):
+        return value.value
+    if isinstance(value, orm.Dict):
+        return value.get_dict()
+    if isinstance(value, orm.List):
+        return value.get_list()
+    raise TypeError(f'value of type {type(value)} is not supported')
 
 
 def get_outputs_dict(process: orm.ProcessNode) -> ty.Dict[str, orm.Node]:
@@ -17,3 +30,32 @@ def get_outputs_dict(process: orm.ProcessNode) -> ty.Dict[str, orm.Node]:
         link_triplet.link_label: link_triplet.node
         for link_triplet in process.get_outgoing(link_type=(LinkType.RETURN, LinkType.CREATE))
     }
+
+
+def get_nested_result(output: ty.Dict[str, orm.Node], key: str) -> ty.Any:
+    """
+    Helper function to retrieve nested outputs as Python data types
+    """
+    nesting_kind = None
+
+    if ':' in key:
+        node_label, output_label = key.split(':')
+        nesting_kind = 'Dict'
+    else:
+        node_label = key
+
+    node_label = node_label.replace('.', '__')
+    node = output[node_label]
+
+    if nesting_kind == 'Dict':
+        if not isinstance(node, orm.Dict):
+            raise TypeError(f'{node} was expected to be orm.Dict, is {type(node)}')
+        keys = output_label.split('.')
+        result = node
+        for key_part in keys:
+            result = result[key_part]
+        result = to_aiida_type(result)
+    else:
+        result = node
+
+    return result
--- tests/test_convergence.py
+++ tests/test_convergence.py
@@ -4,6 +4,7 @@
 """
 Tests for the Convergence engine.
 """
+from aiida import orm
 from aiida_optimize.engines import Convergence
 
 
@@ -29,20 +30,45 @@ def test_convergence_echo_wf(check_optimization):
     )
 
 
-def test_convergence_echo_wf_202(check_error):
+def test_convergence_echo_nested_wf(check_optimization):
     """
-    Test the 202 is_finished_ok failure state of the OptimzationWorkChain with the Convergence engine.
+    Simple test of the OptimizationWorkChain with the Convergence engine.
     """
 
-    check_error(
+    check_optimization(
         engine=Convergence,
         engine_kwargs={
             'input_values': [0, 1, 2, 3, 4.001, 4.002, 5, 4.003, 4.004, 4.005],
             'tol': 1e-1,
-            'input_key': 'x',
-            'result_key': 'result',
-            'convergence_window': 5
+            'input_key': 'a.b.c.d:e.f',
+            'result_key': 'f',
+            'convergence_window': 3
         },
-        func_workchain_name='echo_workfunction',
-        exit_status=202
+        func_workchain_name='EchoNestedValues',
+        xtol=0,
+        ftol=0,
+        x_exact=4.003,
+        f_exact=4.003,
+        evaluate={'x': {
+            'y': orm.Float(0.0)
+        }}
     )
+
+
+# def test_convergence_echo_wf_202(check_error):
+#     """
+#     Test the 202 is_finished_ok failure state of the OptimzationWorkChain with the Convergence engine.
+#     """
+
+#     check_error(
+#         engine=Convergence,
+#         engine_kwargs={
+#             'input_values': [0, 1, 2, 3, 4.001, 4.002, 5, 4.003, 4.004, 4.005],
+#             'tol': 1e-1,
+#             'input_key': 'x',
+#             'result_key': 'result',
+#             'convergence_window': 5
+#         },
+#         func_workchain_name='echo_workfunction',
+#         exit_status=202
+#     )
